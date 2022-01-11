7218640107a537accda889b5cf36e64998858a41
pramodini99 <pramodini.s25@gmail.com>
2022-01-11 17:13:12 +0530

    First Commit

---
+++ C2TCB7_Corejava_Project01/src/Day_06/AbstractionDemo.java
@@ -0,0 +1,54 @@
+package Day_06;
+
+abstract class Bank1{
+	abstract float getRateOfInterest();
+	
+	void display()
+	{
+		System.out.println("Abstract Class:");
+	}
+}
+
+class SBI1 extends Bank1{
+
+	@Override
+	float getRateOfInterest() {
+
+		return 7.0f;
+	}
+	
+}
+class HDFC1 extends Bank1{
+
+	@Override
+	float getRateOfInterest() {
+			return 6.8f;
+	}
+	
+}
+
+class ICICI1 extends Bank1{
+
+	@Override
+	float getRateOfInterest() {
+	
+		return 6.9f;
+	}
+	
+}
+
+public class AbstractionDemo {
+
+	public static void main(String[] args) {
+		SBI1 obj=new SBI1();
+		System.out.println(obj.getRateOfInterest());
+		
+	   Bank1 obj1=new ICICI1();
+	   System.out.println(obj1.getRateOfInterest());
+
+	}
+
+}
+
+
+
---
+++ C2TCB7_Corejava_Project01/src/Day_06/GraphicDemo.java
@@ -0,0 +1,57 @@
+package Day_06;
+
+abstract class GraphicObject{
+
+	int x,y;
+	void moveTo(int newX,int newY)
+	{
+		this.x=newX;
+		this.y=newY;
+	}
+	abstract void draw();
+	abstract void resize();
+}
+	class Rectangle extends GraphicObject
+	{
+		void show()
+		{
+			
+		}
+
+		@Override
+		void draw() {
+
+			
+		}
+
+		@Override
+		void resize() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	class Circle extends GraphicObject
+	{
+
+		@Override
+		void draw() {
+			
+		}
+
+		@Override
+		void resize() {
+			
+		}
+		
+	}
+public class GraphicDemo {
+
+	public static void main(String[] args) {
+		//GraphicObject obj=new GraphicObject();
+		Rectangle obj1=new Rectangle();
+		obj1.show();
+	}
+
+}
